a23ec704cf39fae44fc1989fd5c2abdbfc5bd889
Franklin Graterol <f.graterol01@gmail.com>
2022-11-21 14:23:52 -0500

    Adding NearbySearch  INSTALL STYLED-COMPONENTS

---
+++ frontend/src/Components/Map/NearByPlaces/PlaceElement.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const NearByDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2%;
+`
+
+export const NearbyLi = styled.li`
+    background-color: #fff;
+    border-radius: 10px;
+    color: black;
+    display: flex;
+    justify-content: center ;
+    margin: 2%;
+
+`
