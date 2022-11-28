bd82097a56b581cd3ab62ecf82ca8777928c14b1
Joe <Joevandor96@gmail.com>
2022-11-27 19:42:10 -0800

    Added Dropdown LandmarkStatus and some fetches

--- frontend/src/Components/Map/Places.js
+++ frontend/src/Components/Map/Places.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import ToggleSwitch from '../ToggleSwitch/ToggleSwtich';
 import DatePicker from "react-datepicker"
 import NearByPlaces from "./NearByPlaces/NearByPlaces";
+import LandmarkStatus from "../Landmark/LandmarkStatus"
 
 export default function Places(props){
     const [landmarkName, setLandmarkName] = useState();
@@ -17,6 +18,84 @@ export default function Places(props){
     const [selectedValue, setSelectedValue] = useState();
     
     const [date, setDate] = useState(new Date());
+
+
+    const[currentItin, setCurrentItin] = useState(null)
+    const [landmarkId, setLandmarkId] = useState(null)
+    const [renderLandmark, setRenderLandmark] = useState(false)
+    const API_BASE = "http://localhost:8080/";
+    const API_LANDMARKS = '/landmarks/'
+
+    //does put or post depending on if it exists
+    function sendLandmark() {
+        let landmarkObject = {
+                             image:null,
+                             name:null,
+                             address:null,
+                             type:null,
+                             description:null,
+                             availability:null,
+                                 }
+         if(landmarkId === null){
+         const requestOptions = {
+           method:'POST',
+           headers: { 'Content-Type': 'application/json' },
+           body: JSON.stringify(landmarkObject)
+         };
+     
+         let returnId = fetch(API_BASE + API_LANDMARKS, requestOptions)
+           .then(res => res.json())
+           .then(setLandmarkId(returnId))
+           }
+ 
+         else{
+             landmarkObject = {
+                                 id: landmarkId,
+                                 image:null,
+                                 name:null,
+                                 address:null,
+                                 type:null,
+                                 description:null,
+                                 availability:null,
+                             }
+ 
+                 const requestOptions = {
+                   method:'PUT',
+                   headers: { 'Content-Type': 'application/json' },
+                   body: JSON.stringify(landmarkObject)
+                 };
+             
+                 fetch(API_BASE + API_LANDMARKS + landmarkId, requestOptions)
+                   .then(res => res.json())
+         }
+         }
+
+    function getLandmarks(itineraryId){
+            fetch(API_BASE + API_LANDMARKS + itineraryId)
+            .then(res => res.json())
+            .then(landmarks => {
+                setCurrentItin(landmarks);
+                setRenderLandmark(!renderLandmark);
+            }
+        );
+        }
+
+let landmarks;
+if(renderLandmark){
+    landmarks = currentItin.map((landmark) => 
+                            <div>
+                            <Landmark 
+                            id = {landmark.id}
+                            name = {landmark.name} 
+                            address = {landmark.address} 
+                            type ={landmark.type}
+                            pic = {landmark.pic}
+                            availability = {landmark.availability}
+                            />
+                            <LandmarkStatus id = {landmark.id} />
+                            </div> )
+}
+
 
     const {
         ready, 
